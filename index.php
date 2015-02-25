444cba2415aaf61b2a8c412de2a90fe457e6af58
41485718 <41485718@090029-P33S13.macklabs.br>
2015-02-25 10:00:19 -0300

    Troquei o tamanho e a cor da fonte.

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  * and open the template in the editor.
  */
 
-  echo 12314;
-  echo '<h1 align="center">Brendoon</h1>';
+  echo 41485718;
+  echo '<h1 align="center" style="color:blue">Brendoon Ryos</h1>';
 ?>
